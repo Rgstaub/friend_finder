e09bcdf2abe1bda0c342d68fe16283a1c91f883b
Rgstaub <rgstaub@gmail.com>
2017-08-24 12:11:49 -0400

    pushing to Heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"http://www.freedigitalphotos.net/images/category-images/397.jpg",
     "scores":[
       5,
       1,
